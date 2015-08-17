449dbc9b1d8b074e64efb4cfcc4b41796747ea28
yadongshao <348829719@qq.com>
2015-08-17 21:34:29 +0800

    Update 回文链表.cpp

--- 回文链表.cpp
+++ 回文链表.cpp
@@ -49,3 +49,54 @@ public:
 	}
 
 };
+//上面的代码虽然 完成了功能  但是也破坏了链表的结构 下面是将链表结构比对完  再还原 
+/*
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};*/
+class Palindrome {
+public:
+    
+	bool isPalindrome(ListNode* pHead) {
+		// write code here
+		if(!pHead) return false;
+		if(!pHead->next) return true;
+		ListNode*  fast=pHead->next;
+		ListNode*  slow=pHead;
+		while(fast){
+			if(fast->next) fast=fast->next->next;
+			else
+				break;
+			slow=slow->next;
+		}
+		ListNode *mid=slow;
+		ListNode *c_point=slow;
+		slow=slow->next;
+		c_point->next=NULL;
+		while(slow){
+			ListNode *temp=slow->next;
+			slow->next=c_point;
+			c_point=slow;
+			slow=temp;
+		}
+		bool result=true;
+		ListNode * new_head=c_point;
+		while(c_point!=pHead&&pHead){
+			if(c_point->val!=pHead->val) { result=false;break;} 
+			c_point=c_point->next;
+			pHead=pHead->next;
+		}
+		ListNode * temp_head=NULL;
+		while (new_head!=mid)
+		{
+			ListNode *temp=new_head->next;
+			new_head->next=temp_head;
+			temp_head=new_head;
+			new_head=temp;
+		}
+		mid->next=temp_head;
+		return result;
+	}
+};
