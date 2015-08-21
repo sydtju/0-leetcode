73209c62bb0f8c5af68eba68a794d7025ff7b7a5
yadongshao <348829719@qq.com>
2015-08-21 10:36:49 +0800

    Create 整数转换.cpp

---
+++ 整数转换.cpp
@@ -0,0 +1,23 @@
+/*
+题目描述
+
+编写一个函数，确定需要改变几个位，才能将整数A转变成整数B。
+给定两个整数int A，int B。请返回需要改变的数位个数。
+测试样例：
+10,5
+返回：4
+*/
+class Transform {
+public:
+    int calcCost(int A, int B) {
+        // write code here
+        int c=A^B;
+        int i=0;
+        int f=1;
+        while(f){
+            if(f&c) i++;
+           	f=f<<1;
+        }
+        return i;
+    }
+};
