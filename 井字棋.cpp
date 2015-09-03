525439abaec3686c74fe41b5d78fbfbc8eff52ee
yadongshao <348829719@qq.com>
2015-09-03 21:34:42 +0800

    Update 井字棋.cpp

--- 井字棋.cpp
+++ 井字棋.cpp
@@ -7,8 +7,37 @@
 [[1,0,1],[1,-1,-1],[1,-1,0]]
 返回：true
 
+思路：将数组进行加和
 */
-
+class Board {
+public:
+    bool checkWon(vector<vector<int> > board) {
+        // write code here
+        const int size = board.size();
+        vector<int> tables(8, 0);
+        for(int i = 0; i < size; ++i) {
+ 
+            //row
+            tables[0] += board[i][0];
+            tables[1] += board[i][1];
+            tables[2] += board[i][2];
+ 
+            //col
+            tables[3] += board[0][i];
+            tables[4] += board[1][i];
+            tables[5] += board[2][i];
+ 
+            //corner
+            tables[6] += board[i][i];
+            tables[7] += board[size-i-1][size-i-1];
+        }
+ 
+        if (find(tables.begin(), tables.end(), 3) != tables.end())
+            return true;
+        else
+            return false;
+    }
+};
 class Board {
 public:
     bool checkWon(vector<vector<int> > board) {
